6afcaed80b09fd313ee24768afd7d10ffabb64f4
RaZ <RaZ@192.168.1.64>
2018-06-10 14:17:31 +0200

    premier commit de raz

---
+++ razTest/test/azTest/Solution.java
@@ -0,0 +1,38 @@
+package test.azTest;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+	
+	static String settings = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+
+public static void main(String[] args) {
+	
+	 Scanner in = new Scanner(System.in);
+
+     
+     System.out.println("inserer un STRING");
+     String T = in.nextLine();
+     
+     char tt=T.charAt(0);
+     
+     
+     
+     System.out.println(" String : "+T);
+     System.out.println(" =================================================================================== ");
+     
+     System.out.println(" char "+tt);
+	
+
+     
+     
+
+	}
+
+}
